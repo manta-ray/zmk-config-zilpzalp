24fd349be06184be03615e0b34d70e96e3c9ce6b
manta-ray <luca.helgath@web.de>
2026-01-15 23:32:46 +0100

    Changed layer to to tog

--- config/base_zilpzalp.h
+++ config/base_zilpzalp.h
@@ -82,8 +82,8 @@ combo_##NAME { \
         COMBO(del, &kp DEL, 6 7)
         COMBO(delword, &kp LC(BSPC), 5 7)
 /* Game Layer Combos */
-	COMBO(TOG, &to 5, 24 25 26 27)
-	COMBG(TO0, &to 0, 24 25 26 27)
+	COMBO(TOG, &tog GAM, 24 25 26 27)
+	COMBG(TO0, &tog GAM, 24 25 26 27)
 	COMBG(qg, &kp Q, 2 3)
 	COMBG(t, &kp T, 3 12)
 	COMBG(g, &kp G, 11 12)
